235081c07886e09b0561418017b612063f035826
Zuzana Jeschke <zuz@zuzjes.com>
2019-09-28 20:52:57 +0200

    Add bootstrap, css and sass

--- next.config.js
+++ next.config.js
@@ -9,3 +9,9 @@ module.exports = {
 
 const withImages = require('next-images')
 module.exports = withImages(module.exports)
+
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS(module.exports);
+
+const withSass = require("@zeit/next-sass");
+module.exports = withSass(module.exports);
