ae95179193aa2f6121f26b366c8ebb9cc407f90f
Zuzana Jeschke <zuz@zuzjes.com>
2020-01-25 15:11:55 +0100

    Add document page and make references work

--- pages/index.js
+++ pages/index.js
@@ -55,43 +55,6 @@ export default () => {
           </A>
         </ButtonLike>
       </LayoutWrapper>
-
-      <style jsx>
-        {`
-          .container {
-            padding-right: 15px;
-            padding-left: 15px;
-            margin-right: auto;
-            margin-left: auto;
-            font-family: "Cutive Mono", monospace;
-            max-width: 800px;
-          }
-          h1 {
-            font-size: 3em;
-          }
-          p,
-          a {
-            font-size: 1.2rem;
-          }
-          a {
-            color: grey;
-          }
-          .buttonLike {
-            border: 1px pink solid;
-            padding: ;
-          }
-          .quote {
-            border-left: solid 2px grey;
-            padding-left: 1rem;
-          }
-          ::selection {
-            background: #b5ebde; /* WebKit/Blink Browsers */
-          }
-          ::-moz-selection {
-            background: #b5ebde; /* Gecko Browsers */
-          }
-        `}
-      </style>
       <Gauges gauges_site_id="5c4c99e1e2780409622de4ab" />
       <GoogleAnalytics google_analytics_site_id="UA-133283927-1" />
     </>
--- pages/references.js
+++ pages/references.js
@@ -6,9 +6,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styled from "styled-components";
 
 const LayoutWrapper = styled.div`
+  padding: 1em;
+  margin-right: auto;
+  margin-left: auto;
+  font-family: "Cutive Mono", monospace;
+  max-width: 800px;
   max-width: 45%;
 `;
-export const References = () => (
+
+const Quote = styled.div`
+  border-left: 1.5px solid grey;
+  padding-left: 2em;
+`;
+const References = () => (
   <>
     <LayoutWrapper>
       <a href="https://www.linkedin.com/in/patrik-helta-9a06304b/">
@@ -18,49 +28,18 @@ export const References = () => (
         Zuzka k nám na projekt nastoupila na pozici junior kodéra jako externí
         výpomoc. Velmi rychle se dokázala naučit pracovat s naším vlastním
         frontend frameworkem a nedělalo jí sebemenší problém dodržovat námi
-        nastavené best-practices pro psaní kódu. Především ale oceňuji to, že s
-        týmem aktivně komunikovala a vždy se nad úkoly zamýšlela, proaktivně
-        řešila zadanou práci a nezahazovala problémy pod koberec, což je u
-        juniorních vývojářů opravdu výjimečná vlastnost. I přesto, že byla
-        jediná holka v týmu, okamžitě se s ostatními vývojáři jala hodnotit
-        kvalitu dílenského zpracování a estetičnost zaparkovaných vozidel před
-        budovou. :) Díky za pomoc, Zuzko.
+        nastavené best-practices pro psaní kódu. <br />
+        Především ale oceňuji to, že s týmem aktivně komunikovala a vždy se nad
+        úkoly zamýšlela, proaktivně řešila zadanou práci a nezahazovala problémy
+        pod koberec, což je u juniorních vývojářů opravdu výjimečná vlastnost. I
+        přesto, že byla jediná holka v týmu, okamžitě se s ostatními vývojáři
+        jala hodnotit kvalitu dílenského zpracování a estetičnost zaparkovaných
+        vozidel před budovou. :)
+        <br />
+        Díky za pomoc, Zuzko.
       </Quote>
     </LayoutWrapper>
-    {/* <style jsx>
-        {`
-          .section {
-            padding-top: 1.5rem;
-          }
-          .w-45 {
-            max-width: 45%;
-          }
-          .padd-5 {
-            padding: 5px;
-          }
-          .container {
-            padding-right: 15px;
-            padding-left: 15px;
-            margin-right: auto;
-            margin-left: auto;
-            font-family: "Cutive Mono", monospace;
-            max-width: 800px;
-          }
-          h1 {
-            font-size: 3em;
-          }
-          p,
-          a {
-            font-size: 1.2rem;
-          }
-          a {
-            color: #ff5296;
-          }
-          .quote {
-            border-left: solid 2px grey;
-            padding-left: 1rem;
-          }
-
-      </style> */}
   </>
 );
+
+export default References;
