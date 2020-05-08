c7284410f21c8aa4ee8af2083d1a16ef5782b7e0
Zuzana Jeschke <zuz@zuzjes.com>
2020-05-08 17:42:34 +0200

    edit button layout

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,92 @@
-import Head from 'next/head'
-import Gauges from '../components/Gauges'
-import GoogleAnalytics from '../components/GoogleAnalytics'
-import styled, { createGlobalStyle } from 'styled-components'
+import Head from "next/head";
+import Gauges from "../components/Gauges";
+import GoogleAnalytics from "../components/GoogleAnalytics";
+import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
     font-size: 1.5em;
   }
-`
+`;
 const Container = styled.div`
   padding: 0 2em;
-  font-family: 'IBM Plex Sans', sans-serif;
-  max-width: 800px;
+  font-family: "IBM Plex Sans", sans-serif;
   @media (max-width: 600px) {
     padding: 0 1em;
   }
-`
+`;
 const Header = styled.h1`
   font-size: 2.8em;
   margin-block-end: 0.2em;
-`
-const A = styled.a`
-  color: #088958;
-`
+`;
+// const A = styled.a`
+//   color: #088958;
+// `;
 const Button = styled.a`
   border: 1px solid #088958;
   padding: 0.15em 0.4em;
-  margin: 0.3em 0;
+  margin: 0.3em 0.6em 0.3em 0;
   color: #088958;
   text-decoration: none;
-  display: block;
-  max-width: 300px;
+  display: inline-block;
   &:hover {
     border: 1px solid #088958;
     padding: 0.15em 0.4em;
     background-color: #088958;
     color: white;
   }
-`
+`;
+Button.defaultProps = {
+  target: "_blank"
+};
 
-export default () => {
-  const copyToClipboard = element => {
-    let $temp = $('<input>')
-    $('body').append($temp)
-    $temp.val($(element).text()).select()
-    document.execCommand('copy')
-    $temp.remove()
-  }
-  return (
-    <>
-      <GlobalStyle />
-      <Head>
-        <title>Zuzka Jeschke - zuzjes.com</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Container>
-        <Header>Zuzka Jeschke</Header>
-        <Button href="https://www.linkedin.com/in/zuzjes/">Linkedin</Button>
-        <Button href="https://github.com/zuzjes">Github</Button>
-        <Button href="/references">Reference</Button>
-        <Button href="https://t.me/zuzjes">Telegram</Button>
-        <Button href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</Button>
-        <Button href="tel:+420608540464">+420 608 540 464</Button>
-        <Button href="https://www.facebook.com/zuzka.jeschke">Facebook</Button>
-        <Button href="https://twitter.com/zuzjes">Twitter</Button>
-        <Button href="https://www.instagram.com/zuzjes/">Instagram</Button>
-        <Button href="https://www.instagram.com/zuz_kytky/">
-          Instagram zuz_kytky
-        </Button>
-        <Button href="https://www.instagram.com/jsemnela/">
-          Instagram jsemnela
-        </Button>
-        <p>
-          Číslo účtu: 2501498897 / 2010 (Fio)
-          <br />
-          Revolut:{' '}
-          <A href="https://pay.revolut.com/profile/zuzanakf6">
-            +420 608 540 464
-          </A>
-        </p>
-      </Container>
-      <Gauges gauges_site_id="5c4c99e1e2780409622de4ab" />
-      <GoogleAnalytics google_analytics_site_id="UA-133283927-1" />
-    </>
-  )
-}
+export default () => (
+  <>
+    <GlobalStyle />
+    <Head>
+      <title>Zuzka Jeschke - zuzj.es</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <Container>
+      <Header>Zuzka Jeschke</Header>
+      <Button href="https://zuzjes.com">zuzjes.com</Button>
+      <br />
+      <Button href="https://www.linkedin.com/in/zuzjes/">Linkedin</Button>
+      <Button href="https://github.com/zuzjes">Github</Button>
+      <Button href="https://www.facebook.com/zuzka.jeschke">Facebook</Button>
+      <Button href="https://twitter.com/zuzjes">Twitter</Button>
+      <Button href="https://www.instagram.com/zuzjes/">
+        Instagram @zuzjes
+      </Button>
+      <br />
+
+      <Button href="mailto:zuz@zuzjes.com">zuz@zuzjes.com</Button>
+      <Button href="tel:+420608540464">+420 608 540 464</Button>
+      <Button href="https://t.me/zuzjes">Telegram</Button>
+      <br />
+      <Button href="https://pay.revolut.com/profile/zuzanakf6">
+        Revolut @zuzjes
+      </Button>
+      <Button href="https://paypal.me/zuzjes">Paypal @zuzjes</Button>
+      <br />
+
+      <Button href="https://www.instagram.com/zuz_kytky/">
+        Instagram @zuz_kytky
+      </Button>
+      <Button href="https://www.instagram.com/jsemnela/">
+        Instagram @jsemnela
+      </Button>
+      <p>
+        IČ: 07933215 <br />
+        Číslo účtu: 2501498897 / 2010 (Fio)
+        <br />
+      </p>
+    </Container>
+    <Gauges gauges_site_id="5c4c99e1e2780409622de4ab" />
+    <GoogleAnalytics google_analytics_site_id="UA-133283927-1" />
+  </>
+);
